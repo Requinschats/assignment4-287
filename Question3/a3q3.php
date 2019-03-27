01b896ce9ee7ec85a0aa3e39d54165fa8a5e6379
olivierfradette-roy <olivier.fradette.roy@gmail.com>
2019-03-27 18:24:22 -0400

    .

--- Question3/a3q3.php
+++ Question3/a3q3.php
@@ -25,21 +25,39 @@
     <v-app dark id="inspire">
         <v-toolbar app fixed clipped-left dark>
             <img src="https://picsum.photos/510/300?random" id="headerImage">
-            <v-toolbar-title>Hotel Room Search</v-toolbar-title>
-            <h1 id="clock"> {{time}}</h1>
+            <v-toolbar-title class="title font-weight-light">Hotel Room Search</v-toolbar-title>
+            <h1 class="title font-weight-light" id="clock"> {{time}}</h1>
         </v-toolbar>
         <v-content>
             <v-container fluid fill-height>
-                <v-layout>
-                    <?php
-                    echo "Hello World!";
-                    ?>
+                <v-layout justify-center align-content-center>
+                <v-card light v-if="loginPage" id="loginCard">
+                    <v-layout justify-center pb-3>
+                    <v-card-title style="justify-content: center" pb-3>
+                        <v-icon large left>far fa-user  </v-icon>
+                        <span class="title font-weight-light">Login</span>
+                    </v-card-title>
+                    </v-layout>
+                        <v-text-field outline dark autofocus color="white" label="username" pt-3></v-text-field>
+                        <v-text-field outline dark color="white" label="password" type="password" pt-3></v-text-field>
+                </v-card>
                 </v-layout>
             </v-container>
+            <v-dialog v-model="disclaimerDialog" max-width="400px" dark>
+                <v-card id="disclaimerCard" light>
+                    <v-card-title style="justify-content: center" pb-3>
+                        <v-icon large left> fas fa-user-secret </v-icon>
+                        <span class="title font-weight-light">Disclaimer & Privacy</span>
+                    </v-card-title>
+                    <span class="font-weight-light"> Your information will not be sold or misused. </span>
+                    <span class="font-weight-light"> The creator of this website is not liable nor responsible of any error in the content displayed </span>
+                </v-card>
+            </v-dialog>
         </v-content>
-        <v-footer app fixed light height="50px">
-            <span>&copy; 2019 All images randomly generated from: https://picsum.photos/510/300?random</span>
-            <v-btn dark round @click="disclaimerDialog=true">Privacy/Disclaimer</v-btn>
+        <v-footer app fixed light color="grey lighten-1" height="50px" v-if="showFooter">
+            <span class=" font-weight-light">&copy; 2019 All images randomly generated from: https://picsum.photos/510/300?random</span>
+            <v-btn style="margin-left: 14%" dark round @click="disclaimerDialog=true" light color="grey">Privacy/Disclaimer</v-btn>
+            <v-btn style="margin-left: 42%" fab small @click="showFooter=false"><v-icon color="grey">fas fa-times</v-icon></v-btn>
         </v-footer>
     </v-app>
 </div>
@@ -50,7 +68,9 @@
             return {
                 time: '',
                 date: '',
-                disclaimerDialog: false
+                disclaimerDialog: false,
+                loginPage: true,
+                showFooter: true
             }
         },
         computed:{
